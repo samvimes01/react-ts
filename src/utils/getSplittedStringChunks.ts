55e664df26a1900cd133168a5e30fec709da3f16
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2020-01-10 11:11:03 +0200

    #feature Change get chunks function to separate chunks by words

--- src/utils/getSplittedStringChunks.ts
+++ src/utils/getSplittedStringChunks.ts
@@ -1,12 +1,28 @@
-export default (text: string, chunksCount: number): string[] => {
+const wordSeparators = [' '];
+
+export default function getSplittedStringChunks(text: string, chunksCount: number): string[] {
   const chunks: string[] = [];
   const chunkLength = Math.round(text.length / chunksCount);
-  let chunkEnd = chunkLength;
+  let chunkEnd = getNextWordSeparatorIndex(chunkLength, text, wordSeparators);
   let chunkStart = 0;
   for (let i = 0; i < chunksCount; i++) {
     chunks[i] = text.slice(chunkStart, chunkEnd);
     chunkStart = chunkEnd;
-    chunkEnd = chunkStart + chunkLength < text.length && i + 2 < chunksCount ? chunkStart + chunkLength : text.length;
+    chunkEnd = chunkStart + chunkLength < text.length && i + 2 < chunksCount
+      ? getNextWordSeparatorIndex(chunkStart + chunkLength, text, wordSeparators)
+      : text.length;
   }
   return chunks;
 };
+
+function getNextWordSeparatorIndex(startIndex: number, text: string, wordSeparators: string[]): number {
+  for (let i = startIndex; i < text.length; i++) {
+    if (i + 1 === text.length) {
+      return i;
+    }
+    if (wordSeparators.includes(text.charAt(i))) {
+      return i + 1;
+    }
+  }
+  return startIndex;
+}
